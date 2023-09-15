0627b1f2d343e3b0044936ce02f475aacee5fa4c
AdamSkog <adamskoglund2022@gmail.com>
2023-09-14 22:50:05 -0700

    app development initiated

---
+++ app/main.py
@@ -0,0 +1,54 @@
+from fastapi import FastAPI
+from pydantic import BaseModel, validator
+import joblib
+import pandas as pd
+
+pipe = joblib.load('../pipelines/xgb_pipeline.pkl')
+
+app = FastAPI()
+
+
+class PredictionInput(BaseModel):
+    Age: float
+    Hypertension: int
+    Heart_disease: int
+    Avg_glucose_level: float
+    Bmi: float
+    Stroke: int
+    Sex: str
+    Marriage_status: str
+    Employment: str
+    Residency: str
+    Smoker: str
+
+    @validator('Age', 'Hypertension', 'Heart_disease',
+               'Avg_glucose_level', 'Bmi', 'Stroke')
+    def validate_non_negative(cls, value):
+        if value < 0:
+            raise ValueError("Input values must be non-negative")
+        return value
+
+
+@app.get("/")
+def home():
+    return {'health_check': 'OK'}
+
+
+@app.post('/predict')
+def predict(data: PredictionInput):
+    input_data = pd.DataFrame([{
+        'Age': data.Age,
+        'Hypertension': data.Hypertension,
+        'Heart_disease': data.Heart_disease,
+        'Avg_glucose_level': data.Avg_glucose_level,
+        'Bmi': data.Bmi,
+        'Stroke': data.Stroke,
+        'Sex': data.Sex,
+        'Marriage_status': data.Marriage_status,
+        'Employment': data.Employment,
+        'Residency': data.Residency,
+        'Smoker': data.Smoker
+    }])
+
+    prediction_result = pipe.predict(input_data)
+    return {'prediction': prediction_result}
